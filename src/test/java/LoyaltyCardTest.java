ff022c51ac93506c4bc33a530275c24881aaf751
VanausloosThomas <thomas.vanausloos@cegeka.comgit config --global core.autocrlf inputgit config --global user.email thomas.vanausloos@cegeka.com>
2017-02-23 11:25:12 +0100

    was bussy implementing the addBonusPoints test, not finished

--- src/test/java/LoyaltyCardTest.java
+++ src/test/java/LoyaltyCardTest.java
@@ -1,11 +1,23 @@
+import org.assertj.core.api.Assertions;
+import org.junit.Before;
+import org.junit.Test;
+
 /**
  * Created by paulienl on 23/02/2017.
  */
 public class LoyaltyCardTest {
 
 
-    private String barCode;
-    private String bonusPoints;
+    private LoyaltyCard testCard;
 
     @Before
+    public void setUp(){
+       testCard = new LoyaltyCard("123",0);
+    }
+
+    @Test
+    public void addBonusPoints_startingFromZeroAndAdding10_shouldReturn10(){
+        testCard.add
+        Assertions.assertThat(tes)
+    }
 }
